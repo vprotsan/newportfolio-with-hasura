8df07b97e97f3370704d4e19c754b6c51a042303
Valeria Protsan <valeriyapro@gmail.com>
2018-05-15 15:13:25 -0400

    dropdown improved

--- microservices/ui/app/src/components/Portfolio.js
+++ microservices/ui/app/src/components/Portfolio.js
@@ -1,31 +1,91 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+import Modal from 'react-modal';
+
 import 'react-select/dist/react-select.css';
 
 import PortfolioItem from './PortfolioItem';
-// import PortfolioFilterItem from './PortfolioFilterItem';
+
+
+//Modal
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#page');
+//end modal
 
 class Portfolio extends Component {
 
-  constructor(props){
-      super(props);
+  constructor(){
+      super();
       this.state = {
           categories: [],
-          selectedCategory: '',
-          portfolioItems: []
+          portfolioItems: [],
+          modalIsOpen: false,
+          defaultCategory: 'Select Category',
+          selectValue: 'new-south-wales',
+          selectedOption: '',
+    			disabled: false,
+    			clearable: true,
+    			rtl: false,
       }
+      //modal
+      this.openModal = this.openModal.bind(this);
+      this.afterOpenModal = this.afterOpenModal.bind(this);
+      this.closeModal = this.closeModal.bind(this);
   }
 
-  handleChange = selectedOption => {
-    this.setState({ selectedCategory: selectedOption });
-    console.log(`Selected: ${this.state.selectedCategory.label}`);
+  clearValue (e) {
+		this.select.setInputValue('');
+	}
+
+  switchCountry = e => {
+		var newCat = e.target.value;
+		this.setState({
+			defaultCategory: newCat,
+			selectValue: null,
+		});
+	}
+
+  focusStateSelect = props => {
+		this.select.focus();
+	}
+
+  updateValue = newValue => {
+		this.setState({
+			selectValue: newValue,
+		});
+	}
+
+  //modal
+  openModal() {
+    this.setState({modalIsOpen: true});
   }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+  //end modal
+
 
   componentDidMount(){
       var self = this;
       const EndPoint = `https://data.deterioration37.hasura-app.io/v1/query`;
-      // const imgEndPoint = `http://filestore.deterioration37.hasura-app.io/v1/hooks/public-read`;
       //get categories
       axios.post(EndPoint, {
           "headers": {
@@ -82,21 +142,8 @@ class Portfolio extends Component {
   }
 
   render(){
-
-    // let categories;
-    //
-    // if (this.state.categories.length > 0) {
-    //   categories = this.state.categories.map(cat => ({ value: cat.id,
-    //                                                   label: cat.title
-    //                                                 }));
-    // } else {
-    //   categories = 'No categories';
-    // }
-
-    console.log(this.state.categories);
-    const categories = this.state.categories.map(e => ({value: e.id, label: e.title}))
-
-    const selectedCategory = this.state.selectedCategory;
+    const cat = this.state.categories.map(e => ({value: e.id, label: e.title}))
+    const { selectedOption } = this.state.selectedOption;
 
     return(
       <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
@@ -105,17 +152,53 @@ class Portfolio extends Component {
 
           <div className="row">
             <div className="btn-group col-6 col-md-2 col-lg-2 col-lg-offset-10">
-                <Select
+                {/* <Select
                   name="form-field-name"
-                  value={selectedCategory}
+                  value={selectedOption}
                   onChange={this.handleChange}
-                  options={categories}
-                />
+                  options={cat}
+                /> */}
+
+                <Select
+        					id="state-select"
+        					ref={(ref) => { this.select = ref; }}
+        					onBlurResetsInput={false}
+        					onSelectResetsInput={false}
+        					autoFocus
+        					options={cat}
+        					simpleValue
+        					clearable={this.state.clearable}
+        					name="selected-state"
+        					disabled={this.state.disabled}
+        					value={this.state.selectValue}
+        					onChange={this.updateValue}
+        					rtl={this.state.rtl}
+        				/>
+
             </div>
           </div>
-
-          <PortfolioItem portfolioItems={this.state.portfolioItems}/>
+          <PortfolioItem onClick={this.openModal} portfolioItems={this.state.portfolioItems}/>
         </div>
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
 
       </section>
     )
