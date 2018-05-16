897aae7a73d464eab636beb72461641bb07a2326
Valeria Protsan <valeriyapro@gmail.com>
2018-05-16 16:02:32 -0400

    projects added locally

--- microservices/ui/app/src/components/PortfolioItem.js
+++ microservices/ui/app/src/components/PortfolioItem.js
@@ -11,7 +11,8 @@ const customStyles = {
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -50%)',
+    width                 : '50%'
   }
 };
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
@@ -70,6 +71,7 @@ class PortfolioItem extends Component {
       portfolioItems = this.props.portfolioItems.map((e, index) =>
                   <div key={e.id} onClick={this.val.bind(this, index)} className="card p-3 col-12 col-md-4 col-lg-3 portfolio-item">
                       <div className="card-wrapper">
+                        <div className="fakeoverlay"></div>
                           <div className="card-img">
                               <img className="thumb" src={`https://filestore.deterioration37.hasura-app.io/v1/file/` + e.file_id} title={e.title} alt={e.title} media-simple="true" />
                           </div>
@@ -77,7 +79,7 @@ class PortfolioItem extends Component {
                               <h4 className="card-title mbr-fonts-style mbr-bold display-7 title">{e.title}</h4>
                               <p>{e.content}</p>
                           </div>
-                          <div className="mbr-section-btn"><a href="articles.html" className="btn btn-warning display-4">Details</a></div>
+                          {/* <div className="mbr-section-btn"><a href="articles.html" className="btn btn-warning display-4">Details</a></div> */}
                       </div>
                   </div>
                 )
@@ -85,7 +87,7 @@ class PortfolioItem extends Component {
       portfolioItems = 'No items';
     }
 
-    return( <div className="row row-content justify-content-center">
+    return( <div className="row row-content justify-content-center p-items">
               {portfolioItems}
               <Modal
                     isOpen={this.state.modalIsOpen}
@@ -94,9 +96,26 @@ class PortfolioItem extends Component {
                     style={customStyles}
                     contentLabel="Example Modal"
                     >
-                    <h2>{this.state.modalTitle}</h2>
-                    <button onClick={this.closeModal}>close</button>
-                    <p>{this.state.modalContent}</p>
+
+                    <div className="modal-body">
+                      <div className="row">
+                        <button className="close-modal" onClick={this.closeModal}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+
+                        <div className="col-md-8">
+                            <img className="thumb" src={`https://filestore.deterioration37.hasura-app.io/v1/file/` + this.state.modalImg} title={this.state.modalTitle} alt={this.state.modalTitle} media-simple="true" />
+                        </div>
+
+                        <div className="col-md-4">
+                          <h3 className="my-3">{this.state.modalTitle}</h3>
+                          <p>{this.state.modalContent}</p>
+                          <h3 className="my-3">Project Details</h3>
+                          <ul>
+                            <li><a href="#">on GitHub</a></li>
+                            <li><a href="#">Live preview</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
               </Modal>
             </div> );
 
