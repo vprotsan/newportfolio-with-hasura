2424c35bb92d4f4dc0a4c41beeebf756f4a8cfbc
Valeria Protsan <valeriyapro@gmail.com>
2018-05-17 13:11:08 -0400

    addItem route item added

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import {
   BrowserRouter,
   Route,
+  Redirect,
   Switch
 } from 'react-router-dom';
 
@@ -16,7 +18,63 @@ import NotFound from './components/NotFound';
 import './css/resume.css';
 import './css/style.css';
 
-const App = () => (
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: "",
+      logged: false
+    }
+    this.onlogin = this.onlogin.bind(this)
+    this.handleLoginChange = this.handleLoginChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+  }
+
+  handleLoginChange = e =>
+    this.setState({
+      username: e.target.value
+  })
+
+  handlePasswordChange = e =>
+    this.setState({
+      password: e.target.value
+  })
+
+  onlogin(event) {
+      event.preventDefault();
+      var url = "https://auth.deterioration37.hasura-app.io/v1/login";
+      axios.post(url, {
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "provider": "username",
+        "data": {
+           "username": this.state.username,
+           "password": this.state.password
+       }
+     }).then(data => {
+       this.setState({
+         logged: true
+       })
+       console.log(data);
+       var authToken = data.auth_token
+       window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
+     }).catch(function (error) {
+        console.log(error);
+     })
+  }
+
+render(){
+  let checkIfloggedIn = ()=>{
+    if ( this.state.logged || window.localStorage.getItem('HASURA_AUTH_TOKEN')){
+      return true;
+    }else {
+      return false;
+    }
+  }
+  return(
       <BrowserRouter>
         <div className="App" id="page-top">
           <div className="container-fluid p-0">
@@ -25,14 +83,23 @@ const App = () => (
               <Route exact path="/portfolio" render={() => <Portfolio />}/>
               <Route exact path="/about" render={() => <About />}/>
               <Route exact path="/contact" render={() => <Contact />}/>
-              <Route exact path="/login" render={() => <Login />}/>
+              <Route exact path="/login" render={() => (
+                checkIfloggedIn ?
+                <Redirect to="/addPortfolioItem"/> :
+                <Login
+                  handleLoginChange={this.handleLoginChange}
+                  handlePasswordChange={this.handlePasswordChange}
+                  onLogin={this.onlogin}/>
+                )}
+              />
               <Route component={ NotFound }/>
             </Switch>
           </div>
           <Menu/>
         </div>
       </BrowserRouter>
-
-)
+    )
+  }
+}
 
 export default App;
--- microservices/ui/app/src/components/Login.js
+++ microservices/ui/app/src/components/Login.js
@@ -1,66 +1,63 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 import '../css/login.css';
 
-class Login extends Component {
+const Login = props => (
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: "",
-      password: "",
-      logged: false
-    }
-    this.onlogin = this.onlogin.bind(this)
-    this.handleLoginChange = this.handleLoginChange.bind(this)
-    this.handlePasswordChange = this.handlePasswordChange.bind(this)
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     username: "",
+  //     password: "",
+  //     logged: false
+  //   }
+  //   this.onlogin = this.onlogin.bind(this)
+  //   this.handleLoginChange = this.handleLoginChange.bind(this)
+  //   this.handlePasswordChange = this.handlePasswordChange.bind(this)
+  // }
 
-  handleLoginChange = e =>
-    this.setState({
-      username: e.target.value
-  })
+  // handleLoginChange = e =>
+  //   this.setState({
+  //     username: e.target.value
+  // })
+  //
+  // handlePasswordChange = e =>
+  //   this.setState({
+  //     password: e.target.value
+  // })
 
-  handlePasswordChange = e =>
-    this.setState({
-      password: e.target.value
-  })
+  // onlogin(event) {
+  //   event.preventDefault();
+  //   var url = "https://auth.deterioration37.hasura-app.io/v1/login";
+  //   axios.post(url, {
+  //     "headers": {
+  //         "Content-Type": "application/json"
+  //     },
+  //     "provider": "username",
+  //     "data": {
+  //        "username": this.state.username,
+  //        "password": this.state.password
+  //    }
+  //  }).then(data => {
+  //    this.setState({
+  //      logged: true
+  //    })
+  //    console.log(data);
+  //    var authToken = data.auth_token
+	//    window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
+  //  }).catch(function (error) {
+  //     console.log(error);
+  //  })
+  // }
 
-  onlogin(event) {
-    event.preventDefault();
-    var url = "https://auth.deterioration37.hasura-app.io/v1/login";
-    axios.post(url, {
-      "headers": {
-          "Content-Type": "application/json"
-      },
-      "provider": "username",
-      "data": {
-         "username": this.state.username,
-         "password": this.state.password
-     }
-   }).then(data => {
-     this.setState({
-       logged: true
-     })
-     console.log(data);
-     var authToken = data.auth_token
-	   window.localStorage.setItem('HASURA_AUTH_TOKEN', authToken);
-   }).catch(function (error) {
-      console.log(error);
-   })
-  }
-
-  render(){
-    return(
       <div className="container">
 
-        <form onSubmit={this.onlogin} className="form-signin">
+        <form onSubmit={props.onlogin} className="form-signin">
           <h2 className="form-signin-heading">Please sign in</h2>
           <label htmlFor="inputEmail" className="sr-only">Email address</label>
-          <input name="login" onChange={this.handleLoginChange} type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus="" />
+          <input name="login" onChange={props.handleLoginChange} type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus="" />
           <label htmlFor="inputPassword" className="sr-only">Password</label>
-          <input name="password" onChange={this.handlePasswordChange} type="password" id="inputPassword" className="form-control" placeholder="Password" required="" />
+          <input name="password" onChange={props.handlePasswordChange} type="password" id="inputPassword" className="form-control" placeholder="Password" required="" />
           <div className="checkbox">
             <label>
               <input type="checkbox" value="remember-me" /> Remember me
@@ -70,8 +67,7 @@ class Login extends Component {
         </form>
 
       </div>
-    )
-  }
-}
+
+)
 
 export default Login;
